4bc2de5e8ed14f9c809913911c9ea4aebd3561c2
saravanan121533 <saravanan12153@gmail.com>
2017-02-06 14:43:58 +0530

    modal configured using jquery

--- app.js
+++ app.js
@@ -11,9 +11,26 @@ $(document).ready(function(){
         .addClass('<is-l></is-l>iked');
     });
 
+
     $('.js-add-link').on('click',function(event){
         event.preventDefault();
 
         $('.js-form').toggleClass('is-visible');
     });
+
+    // -----------------------------
+// *Modal
+// ----------------------------
+
+    $('.js-show-modal').on('click', function(event) {
+    event.preventDefault();
+
+    $('.js-modal').addClass('is-visible');
+    $('.js-modal-overlay').addClass('is-visible');
+  });
+
+  $('.js-modal-overlay').on('click', function(event) {
+    $('.js-modal').removeClass('is-visible');
+    $('.js-modal-overlay').removeClass('is-visible');
+  });
 });
